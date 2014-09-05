d3cd43f3691b4851791a1f1e5b95ad258628ae2e
Guido Grassel <guido@guidos-mbp.grassel.org>
2014-09-05 22:22:12 +0300

    initial version as instructed by the tutorial

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var theTextFieldUsername: UITextField!
+    @IBOutlet weak var theTextFieldPassword: UITextField!
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBAction func theVerifyMethod(sender: AnyObject) {
+        var user = "car";
+        var passwrd = "black";
+        
+        if (theTextFieldUsername.text == user) &&
+            (theTextFieldPassword.text == passwrd) {
+                theLabel.text = "You are logged in OK.";
+                theLabel.textColor = UIColor.blackColor();
+                println("You are logged in OK.");
+        } else {
+            theLabel.text = "Your credentials are invalid!";
+            theLabel.textColor = UIColor.redColor();
+            println("Your credentials are invalid!");
+        }
+        theTextFieldPassword.resignFirstResponder();
+        theTextFieldUsername.resignFirstResponder();
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
